b53e3d1aac7d42cc941f454686a20e4f1af878a3
ede1998 <online@erik-hennig.me>
2024-07-20 21:29:27 +0200

    feat: home-manager git config

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -56,7 +56,30 @@
 
   # Enable home-manager and git
   programs.home-manager.enable = true;
-  programs.git.enable = true;
+  programs.git = {
+    enable = true;
+    diff-so-fancy.enable = true;
+    userName = "ede1998";
+    userEmail = "online@erik-hennig.me";
+    signing = {
+      signByDefault = true;
+      key = "A20FCA290932675D";
+    };
+    aliases = {
+      restore = "!f() { git checkout $(git rev-list -n 1 HEAD -- $1)~1 -- $(git diff --name-status $(git rev-list -n 1 HEAD -- $1)~1 | grep ^D | cut -f 2); }; f";
+    };
+    ignores = [
+      ".vscode/"
+    ];
+    extraConfig = {
+      credential.helper = "store --file ~/.config/git/credentials";
+      url = {
+        "ssh://git@github.com" = {
+          insteadOf = "https://github.com";
+        };
+      };
+    };
+  };
 
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
