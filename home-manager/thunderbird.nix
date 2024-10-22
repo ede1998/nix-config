dfb3e07285abdbbb26fd5a4d23a60eaf292c8959
ede1998 <online@erik-hennig.me>
2024-10-22 20:39:07 +0200

    feat: Add Thunderbird local folder module and cfg

--- home-manager/thunderbird.nix
+++ home-manager/thunderbird.nix
@@ -1,4 +1,4 @@
-{ lib, ... }:
+{ lib, config, ... }:
 let
   credentials = lib.importJSON ../secrets/mailbox-dav.json;
   mailboxOrgCalendar = id: {
@@ -21,29 +21,36 @@ let
   };
 in
 rec {
-  accounts.email.accounts.mailbox-org = {
-    primary = true;
-    address = "erik-hennig@mailbox.org";
-    userName = "erik-hennig@mailbox.org";
-    realName = "Erik Hennig";
+  accounts.email = {
+    accounts.mailbox-org = {
+      primary = true;
+      address = "erik-hennig@mailbox.org";
+      userName = "erik-hennig@mailbox.org";
+      realName = "Erik Hennig";
 
-    imap = {
-      host = "imap.mailbox.org";
-      tls.useStartTls = true;
-    };
-
-    smtp = {
-      host = "smtp.mailbox.org";
-      tls.useStartTls = true;
-    };
-
-    thunderbird = {
-      enable = true;
-      settings = id: {
-        "mailnews.default_sort_order" = 2; # descending
-        "mailnews.default_sort_type" = 18; # by date
-        "mail.chat.enabled" = false;
+      imap = {
+        host = "imap.mailbox.org";
+        tls.useStartTls = true;
       };
+
+      smtp = {
+        host = "smtp.mailbox.org";
+        tls.useStartTls = true;
+      };
+
+      thunderbird = {
+        enable = true;
+        settings = id: {
+          "mailnews.default_sort_order" = 2; # descending
+          "mailnews.default_sort_type" = 18; # by date
+          "mail.chat.enabled" = false;
+        };
+      };
+    };
+
+    thunderbird.local-folder = {
+      enable = true;
+      path = "${config.home.homeDirectory}/Documents/mails/backups.sbd";
     };
   };
 
--- modules/home-manager/thunderbird.nix
+++ modules/home-manager/thunderbird.nix
@@ -3,6 +3,7 @@ with lib;
 let
   cfg-calendar = config.accounts.calendar.accounts;
   cfg-contact = config.accounts.contact.accounts;
+  cfg-mail-local = config.accounts.email.thunderbird.local-folder;
 in
 {
   options.accounts = {
@@ -106,6 +107,15 @@ in
           )
         );
     };
+
+    email.thunderbird.local-folder = {
+      enable = mkEnableOption "Thunderbird local folder configuration";
+      path = mkOption {
+        type = types.path;
+        description = "Directory where the mails should be stored locally";
+      };
+    };
+
   };
 
   config = mkIf config.programs.thunderbird.enable {
@@ -130,6 +140,18 @@ in
       mapAttrsToList invalidRemoteTypeCheck cfg-contact;
     programs.thunderbird.settings =
       let
+        genMailLocalFolder = cfg: {
+          # account1/server1 is already added by standard home-manager thunderbird module for local folder
+          "mail.account.account1.server" = "server1";
+          "mail.accountmanager.localfoldersserver" = "server1";
+          "mail.server.server1.directory" = cfg.path;
+          "mail.server.server1.hostname" = "Local Folders";
+          "mail.server.server1.name" = "Local Folders";
+          # Disable adaptive junk mail controls for this account
+          "mail.server.server1.spamLevel" = 0;
+          "mail.server.server1.type" = "none";
+          "mail.server.server1.userName" = "nobody";
+        };
         genCalendar =
           name: cfg:
           let
@@ -195,6 +217,7 @@ in
         ++ (mapAttrsToList genContactList cfg-contact)
         ++ [
           { "calendar.list.sortOrder" = mkSortOrder (sort cmp (calendarPositionsToList cfg-calendar)); }
+          (mkIf cfg-mail-local.enable (genMailLocalFolder cfg-mail-local))
         ]
       );
   };
