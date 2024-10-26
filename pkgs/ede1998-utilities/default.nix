862e938b14168fb1d64ace31c5204aa65f89414e
ede1998 <online@erik-hennig.me>
2024-10-26 22:44:56 +0200

    feat: Add yrename-pictures script

--- pkgs/ede1998-utilities/default.nix
+++ pkgs/ede1998-utilities/default.nix
@@ -1,21 +1,34 @@
 { pkgs }:
 with pkgs;
 let
-  shellApp =
-    name: runtimeInputs:
-    writeShellApplication {
-      inherit name runtimeInputs;
-      text = builtins.readFile ./${name};
-    };
+  shellApp' =
+    extraArgs: name: runtimeInputs:
+    writeShellApplication (
+      {
+        inherit name runtimeInputs;
+
+        text = builtins.readFile ./${name};
+      }
+      // extraArgs
+    );
+  shellApp = shellApp' { };
   apps = [
     (shellApp "yopenany" [
       coreutils
       findutils
       xdg-utils
+      # kdePackages.kde-cli-tools optionally required by xdg-open
     ])
     (shellApp "yrename-no-spaces" [
       coreutils
       findutils
+      gnused
+    ])
+    (shellApp' { bashOptions = [ "nounset" ]; } "yrename-pictures" [
+      coreutils
+      exif
+      findutils
+      gnugrep
       gnused
     ])
   ];
