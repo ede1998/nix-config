807f81e2170a4d3d7899734a1f52fa50f02b9611
ede1998 <online@erik-hennig.me>
2024-11-15 20:47:11 +0100

    feat: Allow unfree package builds without impure

--- flake.nix
+++ flake.nix
@@ -73,11 +73,17 @@
         Command:
         wl-paste | base64 -d | git crypt unlock -
       '';
+      nixpkgsUnfree =
+        system:
+        (import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        });
     in
     {
       # Your custom packages
       # Accessible through 'nix build', 'nix shell', etc
-      packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
+      packages = forAllSystems (system: import ./pkgs (nixpkgsUnfree system));
       # Make `nix develop` possible
       devShells = forAllSystems (system: import ./shell.nix nixpkgs.legacyPackages.${system});
       # Formatter for your nix files, available through 'nix fmt'
