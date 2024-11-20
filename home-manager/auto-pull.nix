ed7213fe0dd0ddc26b27300b549cb6087eff79ee
ede1998 <online@erik-hennig.me>
2024-11-20 21:13:02 +0100

    fix: Make autoPullCfg more resilient

--- home-manager/auto-pull.nix
+++ home-manager/auto-pull.nix
@@ -8,6 +8,7 @@
 let
   github_token = lib.strings.removeSuffix "\n" (builtins.readFile "${secrets}/autoPullNixCfg.cred");
   git = "${pkgs.git}/bin/git";
+  bash = "${pkgs.bash}/bin/bash";
 in
 {
   # Based on https://github.com/Misterio77/nix-config/blob/5b8159e3f2aeadc3c6696f93a7818308b378a7df/home/gabriel/features/cli/nix-index.nix
@@ -30,9 +31,10 @@ in
     Service = {
       Type = "oneshot";
       ExecStartPre = "${git} remote add origin-http https://${github_token}@github.com/ede1998/nix-config.git";
-      ExecStart = "${git} pull origin-http master";
+      # Make pulling work whether branch is checked out or not
+      ExecStart = "${bash} -c '${git} fetch origin-http master:master || ${git} pull origin-http master --rebase'";
       # Remove again to not expose token for longer
-      ExecStartPost = "${git} remote remove origin-http";
+      ExecStopPost = "${git} remote remove origin-http";
       # Can we detect this somehow?
       # i.e. where is the nixos configuration directory located?
       WorkingDirectory = "${config.home.homeDirectory}/nix-config";
