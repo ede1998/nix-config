03a0edea1ae233b2206874490b9e1dcc170ff979
ede1998 <online@erik-hennig.me>
2024-11-25 20:59:46 +0100

    fix: Use tmpfs for /tmp

--- nixos/babbage/disks.nix
+++ nixos/babbage/disks.nix
@@ -99,5 +99,16 @@
         };
       };
     };
+    nodev = {
+      "/tmp" = {
+        fsType = "tmpfs";
+        mountOptions = [
+          "size=32G"
+          "noatime"
+          "mode=1777"
+          "defaults"
+        ];
+      };
+    };
   };
 }
