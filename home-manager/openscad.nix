6e9f6c69ce26b840d82eb6f855445fae1b9f471e
ede1998 <online@erik-hennig.me>
2026-03-12 22:59:21 +0100

    feat(openscad): Add RoundAnything

--- home-manager/openscad.nix
+++ home-manager/openscad.nix
@@ -10,10 +10,22 @@ let
     rev = "v1.0.3";
     sha256 = "sha256-FHHZ5MnOWbWnLIL2+d5VJoYAto4/GshK8S0DU3Bx7O8=";
   };
+
+  round-anything = pkgs.fetchFromGitHub {
+    owner = "Irev-Dev";
+    repo = "Round-Anything";
+    rev = "061fef7c429628808e847696bb345a9b0ec6e279";
+    sha256 = "sha256-xED5fz+VmVv4VJO72PQuExqOtxFdCQ4v4GA99GvA70E=";
+  };
 in
 {
   home.packages = [ pkgs.openscad ];
-  xdg.dataFile."${library-dir}/BOSL" = {
-    source = bosl;
+  xdg.dataFile = {
+    "${library-dir}/BOSL" = {
+      source = bosl;
+    };
+    "${library-dir}/Round-Anything" = {
+      source = round-anything;
+    };
   };
 }
